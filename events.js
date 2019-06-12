864633c277266dc21f3f4226603d822aa6aca6f1
PietroGhg <pietro.ghiglio@mail.polimi.it>
2019-06-12 21:10:14 +0200

    Fix controls

--- events.js
+++ events.js
@@ -15,78 +15,117 @@ keys.push(new Key(76));
 keys.push(new Key(73));
 keys.push(new Key(75));
 
-
+var p1up = 0;
+var p1down = 0;
+var p1left = 0;
+var p1right = 0;
+var p2up = 0;
+var p2down = 0;
+var p2left = 0;
+var p2right = 0;
 
 function action(e){
     // Pressing 'A' on the keybord p1 moves left
     if(e.keyCode == 65 && twoPview)
-        game.p1.dx = -game.p1.speed;
+        p1left = 1;
     else if(e.keyCode == 65 && !twoPview)
-        game.p1.dy = game.p1.speed;
+        p1down = 1;
 
     // Pressing 'D' on the keybord p1 moves right
     if(e.keyCode == 68 && twoPview)
-        game.p1.dx = game.p1.speed;
+        p1right = 1;
     else if(e.keyCode == 68 && !twoPview)
-        game.p1.dy = -game.p1.speed;
+        p1up = 1;
 
     // Pressing 'W' on the keybord p1 moves up
     if(e.keyCode == 87 && twoPview)
-        game.p1.dy = -game.p1.speed;
+        p1up = 1;
     else if(e.keyCode == 87 && !twoPview)
-        game.p1.dx = -game.p1.speed;
+        p1left = 1;
 
     // Pressing 'S' on the keybord p1 moves down
     if(e.keyCode == 83 && twoPview)
-        game.p1.dy = game.p1.speed;
+        p1down = 1;
     else if(e.keyCode == 83 && !twoPview)
-        game.p1.dx = game.p1.speed;
+        p1right = 1;
 
     // Pressing 'J' on the keybord p2 moves left
     if(e.keyCode == 74 && twoPview)
-        game.p2.dx = game.p2.speed;
+        p2left = 1;
     else if(e.keyCode == 74 && !twoPview)
-        game.p2.dy = game.p2.speed;
+        p2up = 1;
 
     // Pressing 'L' on the keybord p2 moves right
     if(e.keyCode == 76 && twoPview)
-        game.p2.dx = -game.p2.speed;
+        p2right = 1;
     else if(e.keyCode == 76 && !twoPview)
-        game.p2.dy = -game.p2.speed;
+        p2down = 1;
 
     // Pressing 'I' on the keybord p2 moves up
     if(e.keyCode == 73 && twoPview)
-        game.p2.dy = game.p2.speed;
+        p2up = 1;
     else if(e.keyCode == 73 && !twoPview)
-        game.p2.dx = -game.p2.speed;
+        p2right = 1;
 
     // Pressing 'K' on the keybord p2 moves down
     if(e.keyCode == 75 && twoPview)
-        game.p2.dy = -game.p2.speed;
+        p2down = 1;
     else if(e.keyCode == 75 && !twoPview)
-        game.p2.dx = game.p2.speed;
+        p2left = 1;
+    
+    calculateSpeed();
 }
 
 function release(e){
-    if ((e.keyCode == 65 || e.keyCode == 68) && twoPview)
-        game.p1.dx = 0;
-    else if ((e.keyCode == 65 || e.keyCode == 68) && !twoPview)
-        game.p1.dy = 0;
+    //A
+    if(e.keyCode == 65 && twoPview)
+        p1left = 0;
+    else if(e.keyCode == 65 && !twoPview)
+        p1down = 0;
 
-    if ((e.keyCode == 87 || e.keyCode == 83) && twoPview)
-        game.p1.dy = 0;
-    else if ((e.keyCode == 87 || e.keyCode == 83) && !twoPview)
-        game.p1.dx = 0;
+    // Pressing 'D' on the keybord p1 moves right
+    if(e.keyCode == 68 && twoPview)
+        p1right = 0;
+    else if(e.keyCode == 68 && !twoPview)
+        p1up = 0;
 
-    if ((e.keyCode == 74 || e.keyCode == 76) && twoPview)
-        game.p2.dx = 0;
-    else if ((e.keyCode == 74 || e.keyCode == 76) && !twoPview)
-        game.p2.dy = 0;
+    // Pressing 'W' on the keybord p1 moves up
+    if(e.keyCode == 87 && twoPview)
+        p1up = 0;
+    else if(e.keyCode == 87 && !twoPview)
+        p1left = 0;
 
-    if ((e.keyCode == 73 || e.keyCode == 75) && twoPview)
-        game.p2.dy = 0;
-    else if ((e.keyCode == 73 || e.keyCode == 75) && !twoPview)
-        game.p2.dx = 0;
+    // Pressing 'S' on the keybord p1 moves down
+    if(e.keyCode == 83 && twoPview)
+        p1down = 0;
+    else if(e.keyCode == 83 && !twoPview)
+        p1right = 0;
+    
+        // Pressing 'J' on the keybord p2 moves left
+    if(e.keyCode == 74 && twoPview)
+        p2left = 0;
+    else if(e.keyCode == 74 && !twoPview)
+        p2up = 0;
+
+    // Pressing 'L' on the keybord p2 moves right
+    if(e.keyCode == 76 && twoPview)
+        p2right = 0;
+    else if(e.keyCode == 76 && !twoPview)
+        p2down = 0;
+
+    // Pressing 'I' on the keybord p2 moves up
+    if(e.keyCode == 73 && twoPview)
+        p2up = 0;
+    else if(e.keyCode == 73 && !twoPview)
+        p2right = 0;
+
+    // Pressing 'K' on the keybord p2 moves down
+    if(e.keyCode == 75 && twoPview)
+        p2down = 0;
+    else if(e.keyCode == 75 && !twoPview)
+        p2left = 0;
+    
+    calculateSpeed();
 }
 
 function checkPress(e){
@@ -105,4 +144,14 @@ function checkPress(e){
 
 function changeView(){
     twoPview = !twoPview;
+}
+
+function calculateSpeed(){
+    game.p1.dx = game.p1.speed*p1right - game.p1.speed*p1left;
+    game.p1.dy = -game.p1.speed*p1up + game.p1.speed*p1down;
+    game.p1.normalizeSpeed();
+
+    game.p2.dx = -game.p2.speed*p2right + game.p2.speed*p2left;
+    game.p2.dy = game.p2.speed*p2up - game.p2.speed*p2down;
+    game.p2.normalizeSpeed();
 }
